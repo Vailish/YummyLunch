d45a32ff53a45088ae565312372338a2f07d9169
vailish <joyksj94@gmail.com>
2024-08-12 09:00:05 +0900

    add sql xml

--- yummyLunch/src/main/java/egovframework/yummylunch/service/RestaurantDAO.java
+++ yummyLunch/src/main/java/egovframework/yummylunch/service/RestaurantDAO.java
@@ -1,9 +1,20 @@
 package egovframework.yummylunch.service;
 
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
 import org.egovframe.rte.psl.dataaccess.EgovAbstractMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class RestaurantDAO extends EgovAbstractMapper {
-
+	
+	public List<RestaurantVO> selectRestrauntList() throws SQLException {
+		return selectList("selectRestrauntList");
+	}
+	
+	public int insertRestraunt(String ㅇㅅㅇ) throws SQLException, IOException{
+		return insert(null, ㅇㅅㅇ);
+	}
 }
---
+++ yummyLunch/target/yummyLunch-1.0.0/WEB-INF/jsp/yummylunch/test.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<h1>hi</h1>
+
+<form action="/test/nb" method="POST">
+	<select name="number">
+		<option value="1">원</option>
+		<option value="2">투</option>
+		<option value="3">쓰리</option>		
+	</select>
+	<input type="submit" value="submit">
+</form>
+</body>
+</html>
